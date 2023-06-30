7a552f045e41f8780dd4a0b310dbd40f3e53f017
Jeevan <jeevan@Jeevans-MacBook-Air.local>
2023-06-30 20:59:13 +0530

    JWT added

---
+++ src/main/java/com/spring/security/Security/controller/AuthenticationController.java
@@ -0,0 +1,17 @@
+package com.spring.security.Security.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.security.Security.Bean.AuthRequest;
+import com.spring.security.Security.JWT.JwtService;
+
+@RestController
+public class AuthenticationController {
+	
+}
+		
--- src/main/java/com/spring/security/Security/controller/CookController.java
+++ src/main/java/com/spring/security/Security/controller/CookController.java
@@ -9,12 +9,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.security.Security.Bean.AuthRequest;
+import com.spring.security.Security.JWT.JwtService;
 import com.spring.security.Security.dao.UserDAO;
 import com.spring.security.Security.entity.UserEntity;
 
@@ -22,8 +29,12 @@ import com.spring.security.Security.entity.UserEntity;
 @RestController
 public class CookController {
 	
-@Autowired
-private UserDAO uBean;
+	@Autowired
+	private UserDAO uBean;
+	@Autowired
+	private JwtService jwtService;
+	@Autowired
+	private AuthenticationManager authenticationManager;
 	
 	@RequestMapping(value="/" ,method=RequestMethod.GET)
 	public String index()
@@ -51,4 +62,18 @@ private UserDAO uBean;
 		return "User";
 	}
 	
+	@RequestMapping(value="/auth",method=RequestMethod.POST)
+	public String authenticateAndGetToken(@RequestBody AuthRequest authRequest)
+	{
+		Authentication authentication=authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUserName(),authRequest.getPassword()));
+		if(authentication.isAuthenticated())
+		{
+			return jwtService.generateToken(authRequest.getUserName());
+		}
+		else
+		{
+			throw new UsernameNotFoundException("Invalid credentials");	
+		}
+		
+	}
 }
